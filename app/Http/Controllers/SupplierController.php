bb3ee5f1d5d3c22b6491f7844ceada455afd6c4a
nabilahzalfa <nabilah.zalfa26@gmail.com>
2022-03-17 17:25:06 +0700

    kuis 1 progress 4

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Supplier;
+
 class SupplierController extends Controller
 {
     public function supplier(){
+        $all_supplier = Supplier::all();
+
         return view('supplier')
-        ->with('title', 'Dz Fashion - Supplier');
+        ->with('title', 'Dz Fashion - Supplier')
+        ->with('all_supplier', $all_supplier);
     }
 }
