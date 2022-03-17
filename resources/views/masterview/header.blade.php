f0250b61f929a85529b3c182a5062f9b929103ac
rifarosyida <rifarosyida2002@gmail.com>
2022-03-17 21:54:16 +0700

    new asset

--- resources/views/masterview/header.blade.php
+++ resources/views/masterview/header.blade.php
@@ -13,31 +13,11 @@
                            
                             <li class="scroll-to-section"><a href="{{url('/home')}}" class="{{ ($title == "Dz Fashion - Home" )? "active" : "" }}">Home</a></li>
                             <li class="scroll-to-section"><a href="{{url('/supplier')}}" class="{{ ($title == "Dz Fashion - Supplier" )? "active" : "" }}">Supplier</a></li>
-                           	
-                        <!-- 
-                            <li class="submenu">
-                                <a href="javascript:;">Drop Down</a>
-                                <ul>
-                                    <li><a href="#">Drop Down Page 1</a></li>
-                                    <li><a href="#">Drop Down Page 2</a></li>
-                                    <li><a href="#">Drop Down Page 3</a></li>
-                                </ul>
-                            </li>
-                        -->
+                          
                             <li class="scroll-to-section"><a href="{{url('/barang')}}" class="{{ ($title == "Dz Fashion - Barang" )? "active" : "" }}">Barang</a></li>
                             <li class="scroll-to-section"><a href="{{url('/pegawai')}}" class="{{ ($title == "Dz Fashion - Pegawai" )? "active" : "" }}">Pegawai</a></li>
                             <li class="scroll-to-section"><a href="{{url('/pelanggan')}}" class="{{ ($title == "Dz Fashion - Pelanggan" )? "active" : "" }}">Pelanggan</a></li>  
-                            <!-- <li class="submenu">
-                                <a href="javascript:;">Features</a>
-                                <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
-                                    <li><a href="#">Features Page 4</a></li>
-                                </ul>
-                            </li> -->
-                            <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
-                            <!-- <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li>  -->
+                            
                         </ul>        
                         <a class='menu-trigger'>
                             <span>Menu</span>
